af84bde62703143c12ef52ad6cfafbaa5946bcc1
amy-monroe <amy.monroe97@gmail.com>
2023-12-06 15:48:55 +0000

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # File Metadata Microservice
 
-This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+This project was made using FreeCodeCamp's boilerplate for the File Metadata Microservice project. I wrote my own backend code in order to replicate FreeCodeCamp's working demo.g
+
+ Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+//setting up multer for uploading files
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+// handles uploading files and then returns the metadata
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  // file metadata
+  const metadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
 
-
-
+//responds with the metadata
+  res.json(metadata);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
